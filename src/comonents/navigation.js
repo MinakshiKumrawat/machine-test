28ae7009eacddcde1e65236a98c68259e25822fe
MinakshiKumrawat <49302782+MinakshiKumrawat@users.noreply.github.com>
2021-10-10 00:39:59 +0530

    no

--- src/comonents/navigation.js
+++ src/comonents/navigation.js
@@ -1,28 +1,46 @@
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
-        return<>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-           <div className="container">
-             <Link className="navbar-brand" to="/home">LOGO</Link>
-             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/home">Home</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/task">Task</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/user">User</Link>
-                  </li>
-                </ul>
-              </div>
-           </div>    
-          </nav>
-        </>
-}
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/home">
+            LOGOO
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/home">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/task">
+                  Task
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/user">
+                  User
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 export default Navigation;
