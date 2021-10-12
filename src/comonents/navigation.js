d27a8162495e5ba61c254c166f04353c0d11dd7a
MinakshiKumrawat <minakshikumrawat14@gmail.com>
2021-10-12 20:35:55 +0530

    update

--- src/comonents/navigation.js
+++ src/comonents/navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
            <div className="container">
              <Link className="navbar-brand" to="/home">LOGO</Link>
              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon "></span>
+                <span className="navbar-toggler-icon   "></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
